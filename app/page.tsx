2375712bdfff6680936ffd242921ba21a5ad6d8a
marla-tumenjargal <marlatumenjargal@berkeley.edu>
2026-08-05 04:09:01 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default function HomePage() {
 
         <p>
           I'm an undergraduate @ UC Berkeley studying computer science, passionate about all-things machine learning, software dev, and creative tech. 
-          At the moment, I'm building Wisp and overseeing operations at Launchpad. 🚀 <a href=""></a>
+           <a href=""></a>
           
         </p>
       
